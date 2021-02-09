84411b9d8c9ffc1d8162ba03acd3cf2d04ec37ef
Jan Koll <mail@jankoll.de>
2021-02-09 17:40:12 +0100

    Add REST API Packages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { HTTP } from '@ionic-native/http/ngx';
+// import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,6 +13,36 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class HomePage {
   route: Router;
 
-  constructor() {}
+  content: [];
+
+  // constructor() {}
+  constructor(private http: HTTP) {
+    this.http.useBasicAuth('mail@example.de', 'Raute123');
+
+    this.http.get('http://localhost:8000/rest/main', {}, {})
+    .then(data => {
+
+      // console.log(data.status);
+      this.content = JSON.parse(data.data); // data received by server
+      // console.log(data.headers);
+      console.log(this.content);
+      console.log("test");
+
+
+
+    })
+    .catch(error => {
+
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+
+    });
+
+  }
+
+
+
+
 
 }
